80ade798dec3f516bce032c3e7fcad240b239a1f
Yassine El hassani <yassine.ds.404@gmail.com>
2025-02-20 15:48:24 +0100

    User Controller Class

---
+++ app/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+class UserController {
+    public function index() {
+        echo "User Index";
+    }
+
+    public function login() {
+        echo "User Login";
+    }
+
+    public function register() {
+        echo "User Register";
+    }
+}
+
+
+?>
