6679084a88abcdb2178bf1675ab2397aa65d4270
harit <harit@gmail.com>
2025-02-20 16:53:54 +0100

    rogister

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once './vendor/autoload.php';
 use App\Router\Router;
 
 $uri = $_SERVER['REQUEST_URI'];
+$uri = str_replace('/Blog_Project_MVC-NATIVE-','',$uri);
 
 $routes = new Router();
 $routes->dispatch($uri);
