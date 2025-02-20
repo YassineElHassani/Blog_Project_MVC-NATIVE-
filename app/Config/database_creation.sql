822968cb20ecf0d1f60828879107d38fe9e2ed70
Yassine El hassani <yassine.ds.404@gmail.com>
2025-02-20 16:24:06 +0100

    SQL Query for database creation

---
+++ app/Config/database_creation.sql
@@ -0,0 +1,22 @@
+-- Create Database
+CREATE DATABASE blog_mvc;^
+
+USE blog_mvc;
+-- Create Users Table
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('visitor', 'admin') NOT NULL DEFAULT 'visitor'
+);
+
+-- Create Articles Table
+CREATE TABLE articles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
